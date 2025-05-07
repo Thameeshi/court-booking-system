986c831a0f4c7a4800639331645c334095d0506d
Hayesha Perera <hayeshahp6@gmail.com>
2025-05-07 14:38:49 +0530

    resolved merge conflict

--- court-booking-system-contract/src/Constants/Tables.js
+++ court-booking-system-contract/src/Constants/Tables.js
@@ -1,12 +1,6 @@
 export const Tables = {
-<<<<<<< HEAD
-	USER: "User",
-	COURT: "Court",
-	COURT_BOOKING: "bookings",
-	BOOKING_REQUEST: "BookingRequest",
-=======
+
     USER: "User",
     COURT: "Court",
     BOOKING: "bookings" // Ensure this matches the table name in initDB.js
->>>>>>> 1f1e6f51ba62a1cfa0abd0381d6d20cbc454e21c
-};
+}
